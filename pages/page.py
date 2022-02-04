a1f2b7b9c1e491b3ca7400028aadaaa957fb5724
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-04 07:50:40 -0500

    Update page.py

--- pages/page.py
+++ pages/page.py
@@ -8,6 +8,7 @@ class BasePage(object):
     """
     Base class to initialize the base page that will be called from all pages
     """
+    
     def __init__(self, url, headless):
         """
         Params:
